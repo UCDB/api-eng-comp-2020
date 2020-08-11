6f385d31b780063ae975b90aa1077b2288ff09f0
thiago.lodi <thiago.lodi@azi.com.br>
2020-08-11 00:02:53 -0400

    configuration to connect with db

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import helmet from 'helmet';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import { log } from 'console';
 import router from './router';
+import database from './database';
 
 dotenv.config();
 
 const PORT : number = parseInt(process.env.PORT as string, 10) || 3333;
+const MONGO_URI : string = process.env.MONGO_URI || '';
 
 const app = express();
 
@@ -16,7 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+database.connect(MONGO_URI);
+
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`API Listening on port ${PORT}`);
+  log(`API Listening on port ${PORT}`);
 });
